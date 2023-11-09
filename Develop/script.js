debb3e4c90c0d04657760b48b1966fdd36b7146d
Jose Silva <js38902@gmail.com>
2023-11-08 21:39:27 -0500

    javascrit for work planner

--- Develop/script.js
+++ Develop/script.js
@@ -1,23 +1,60 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+
+$(document).ready(function () {
+  // Get the current date and time using Day.js
+  var currentDate = dayjs().format('MMMM D, YYYY');
+  var currentTime = dayjs().format('H'); // 24-hour format
+
+//current date in the header
+var currentDate = dayjs().format('MMMM D, YYYY');
+document.getElementById('currentDay').textContent = currentDate;
+
+
+// Get the current time, will make the past, present, and future section functionable 
+var currentTime = new Date().getHours();
+
+// Get all elements with the class "time-block"
+var timeBlocks = document.querySelectorAll('.time-block');
+
+// Loop through each time block
+timeBlocks.forEach(function (timeBlock) {
+  var blockId = timeBlock.id;
+  var blockTime = parseInt(blockId.split('-')[1]);
+
+
+  if (blockTime < currentTime) {
+    timeBlock.classList.remove('future', 'present');
+    timeBlock.classList.add('past');
+
+  } else if (blockTime === currentTime) {
+    timeBlock.classList.remove('past', 'future');
+    timeBlock.classList.add('present');
+
+  } else {
+    timeBlock.classList.remove('past', 'present');
+    timeBlock.classList.add('future');
+  }
+});
+
+
+  // Function to save tasks to local storage
+  function saveTask() {
+    var blockId = $(this).parent().attr('id');
+    var taskText = $(this).siblings('.description').val();
+
+    localStorage.setItem(blockId, taskText);
+  }
+
+  // Load saved tasks from local storage
+  //"this" reffering to the current "time-block"
+  $('.time-block').each(function () {
+    var blockId = $(this).attr('id');
+    var savedTask = localStorage.getItem(blockId);
+
+    if (savedTask) {
+      $(this).find('.description').val(savedTask);
+    }
+  });
+
+  // Add click event listeners to save buttons
+  $('.saveBtn').on('click', saveTask);
 });
